c2cd22d504ffd5e709fe114a6834da9f268a0556
Samir Phuyal <samirphuyal65@gmail.com>
2020-09-12 20:34:51 +0545

    first commit

---
+++ blogs/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Blog(models.Model):
+    """
+    Write blog object model fields
+    """
+    pass
+
---
+++ events/admin.py
@@ -0,0 +1,58 @@
+from django.contrib import admin
+from .models import Category, Event, File, Organizer, Person, Program, Schedule, Speaker
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Customize Category Section Admin Panel
+    """
+    pass
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    """
+    Customize Event Section Admin Panel
+    """
+    pass
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    """
+    Customize File Section Admin Panel
+    """
+    pass
+
+@admin.register(Organizer)
+class OrganizerAdmin(admin.ModelAdmin):
+    """
+    Customize Organizer Section Admin Panel
+    """
+    pass
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    """
+    Customize Person Section Admin Panel
+    """
+    pass
+
+@admin.register(Speaker)
+class SpeakerAdmin(admin.ModelAdmin):
+    """
+    Customize Speaker Section Admin Panel
+    """
+    pass
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    """
+    Customize Schedule Section Admin Panel
+    """
+    pass
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    """
+    Customize Program Section Admin Panel
+    """
+    pass
