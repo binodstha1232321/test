fd62de603ebc808801c21cb27c2e6f9fcbab6c43
gagan <toshiro.limbu01@gmail.com>
2020-09-16 12:53:00 +0545

    --- Auto-Git Commit ---

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Blog
 
+
+
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    """
-    Customize blog section admin panel
-    """
-    pass
+    list_display = ('title', 'description', 'image', 'content', 'tags', 'date_added', 'date_edited')
+    search_fields = ['title', 'content']
+    list_display_links = ('title')
+    list_filter = ('date_added')
+
+admin.site.site_header = "Admin Panel"
+admin.site.site_title = "Admin Panel"
