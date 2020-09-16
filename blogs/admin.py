e49732a7f756f1b87ff489a3f4ec6b4b36612afc
gagan <toshiro.limbu01@gmail.com>
2020-09-16 13:16:27 +0545

    add BlogAdmin class in admin file of blogs folder

--- blogs/admin.py
+++ blogs/admin.py
@@ -5,10 +5,11 @@ from .models import Blog
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'image', 'content', 'tags', 'date_added', 'date_edited')
+    list_display = ('title', 'description', 'image', 'date_added', 'date_edited')
     search_fields = ['title', 'content']
-    list_display_links = ('title')
-    list_filter = ('date_added')
+    list_display_links = ('title',)
+    list_filter = ('date_added',)
+    ordering = ['title']
 
 admin.site.site_header = "Admin Panel"
 admin.site.site_title = "Admin Panel"
