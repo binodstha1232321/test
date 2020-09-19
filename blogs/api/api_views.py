b821e3778de177c0648e4f25b09300b92668980f
gagan <toshiro.limbu01@gmail.com>
2020-09-19 19:41:38 +0545

    creating and BlogViewSet class in blog api_views

--- blogs/api/api_views.py
+++ blogs/api/api_views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from event.blogs.api.serializers import UserSerializer
+from event.blogs.api.serializers import UserSerializer, BlogSerializer
 from event.blogs.api import serializers
 from rest_framework import viewsets, permissions
 from event.blogs.models import Blog
@@ -8,5 +8,18 @@ from django.contrib.auth.models import User
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
+    
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class BlogViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    """
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                        IsOwnerOrReadOnly
+                        ]
