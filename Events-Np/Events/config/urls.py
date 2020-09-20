5234948ec074c0d8eeb3ecd04757370353fc4d92
binod <stha.binod1000@gmail.com>
2020-09-20 19:11:46 +0545

    chanages in router

--- Events-Np/Events/config/urls.py
+++ Events-Np/Events/config/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth.models import User
-
+from .api_router import router
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include(router.urls)),
+    path('api/', include(router.urls)),
     path('',include('events.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
